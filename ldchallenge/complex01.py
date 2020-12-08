3d1e378f8ed4f930e7619ceca4c00e1f636330a6
Jamal <doesntreallymatter@gmail.com>
2020-12-08 22:46:54 +0000

    LandD

---
+++ ldchallenge/complex01.py
@@ -0,0 +1,16 @@
+# create a list called list5
+list5 = ["great dane", "frenchie", "mastiff"]
+# display list5
+print(list1)
+# create a list called list6
+list6 = ["lion", "cat", "panther"]
+#display list6
+print(list6)
+# create a list called list7
+list7 = list5 + list6
+# display list7
+print(list7)
+
+
+
+
